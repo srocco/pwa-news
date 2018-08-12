95bfe176d018ff799d2cfdfcf000e5c29b328f02
srocco <smrocco@gmail.com>
2018-08-12 19:57:38 -0300

    added image optimization

--- src/app/news/mock.news.ts
+++ src/app/news/mock.news.ts
@@ -5,7 +5,7 @@ export const NEWS: News[] = [
     id: 1,
     title: 'La Linux Foundation celebra la compra de GitHub por parte de Microsoft',
     introText: 'Según Jim Zemlin, Director Ejecutivo de la Linux Foundation, "son muy buenas noticias para el mundo del open source, deberíamos celebrar esta inteligente jugada de Microsoft".',
-    image: 'assets/news_1.png',
+    image: 'assets/news_1-min.jpg',
     content: 'La semana pasada, llamaba nuestra atención la noticia de la compra de GitHub por parte de Microsoft por la nada despreciable cifra de 7.500 millones de dólares. Y lo otro que también llamó la atención fue la reacción de la comunidad open source, mucho escepticismo, miles de repositorios mudadándose a GitLab y preocupación por el futuro de la plataforma referente para desarrolladores de todo el mundo....'
   },
 
@@ -14,7 +14,7 @@ export const NEWS: News[] = [
     id: 2,
     title: 'Predicciones 5 en 5 de IBM Research',
     introText: 'Cada año IBM Research presenta algunos de los avances que surgen de sus laboratorios globales. Veamos las predicciones 2018 para los próximos 5 años.',
-    image: 'assets/news_2.png',
+    image: 'assets/news_2-min.jpg',
     content: 'Según el Jefe de IBM Research, Arvind Krishna, estas cinco tecnologías reformarán fundamentalmente los negocios y la sociedad en los próximos cinco años.....'
   },
 
@@ -22,7 +22,7 @@ export const NEWS: News[] = [
     id: 3,
     title: 'Jack Ma desconfía de bitcoin pero no de blockchain',
     introText: '"Bitcoin podría ser una burbuja, pero blockchain podría cambiar nuestro mundo"',
-    image: 'assets/news_2.png',
+    image: 'assets/news_3-min.jpg',
     content: 'No es la primera vez que el cofundador y CEO de Alibaba se manifiesta escéptico respecto al potencial de bitcoin pero, separando las aguas, sostiene una postura opuesta cuando se refiere a blockchain......'
   },
 
@@ -30,7 +30,7 @@ export const NEWS: News[] = [
     id: 4,
     title: 'Microsoft Build Live 2018',
     introText: 'Mirá las keynotes de Satya Nadella, Scott Guthrie, Joe Belfiore y enterate de los anuncios y novedades presentadas en el evento anual mas importante de Microsoft para los desarrolladores.',
-    image: 'assets/news_4.jpg',
+    image: 'assets/news_4-min.jpg',
     content: ''
   },
 
@@ -38,7 +38,7 @@ export const NEWS: News[] = [
     id: 5,
     title: 'H+ ¿Los transhumanistas menosprecian a los humanos?',
     introText: 'Los avances tecnológicos acelerados están generando cambios en lo que somos ¿Por qué algunos pensadores sostienen que vamos hacia un estado post humano del Ser?',
-    image: 'assets/news_5.jpg',
+    image: 'assets/news_5-min.jpg',
     content: '¿Superaremos nuestras limitaciones biológicas con tecnología? Perder nuestra humanidad, ¿nos hará mejores o peores? ¿Por qué somos “grandes misterios para nosotros mismos”?'
   },
 
